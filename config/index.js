dd6db12b2a41f1419154fb87769b9db6c2386eda
Alberte Vieites <albertevieites@tutanota.com>
2022-05-21 10:00:05 +0200

    Added line to have available the login on Herolu

--- config/index.js
+++ config/index.js
@@ -27,6 +27,7 @@ module.exports = (app) => {
   app.use(logger("dev"));
 
   // To have access to `body` property in the request
+  app.set("trust proxy", 1);
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
